dba4b36de09516e739a543f5db3376e46056e1ca
Collin Chiu <g35151@yahoo.com.tw>
2021-06-27 17:25:51 +0800

    3rd cat

--- Cat.java
+++ Cat.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Cat  implements Serializable{
 	private static final long serialVersionUID = 2L;
 	private String name;
+	private String nickname2;
 	
 	public Cat(String name) {
 		this.name = name;
